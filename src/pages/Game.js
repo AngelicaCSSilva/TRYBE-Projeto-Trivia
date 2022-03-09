954da50febbc353e515d05a86554ef0337783eef
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-09 19:47:34 -0300

    Criada página genérica Game

---
+++ src/pages/Game.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  // handleChanges = ({ target }) => {
+  //   const { name, value } = target;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
+
+  // handleClick = () => {
+  //   const { email } = this.state;
+  //   const { userClick, history } = this.props;
+  //   userClick(email);
+  //   history.push('/play');
+  // }
+  render() {
+    return (
+      <section>
+        <p> AAAA </p>
+      </section>
+    );
+  }
+}
+
+export default Game;
