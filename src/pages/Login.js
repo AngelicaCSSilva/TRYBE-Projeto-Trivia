8b4948a6d30cfd04415828f7f15819be8c4e54b3
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-09 15:36:38 -0300

    criando pagina login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import logo from '../trivia.png';
+// import { loginAction } from '../actions';
+
+class Login extends Component {
   handleChanges = ({ target }) => {
     const { name, value } = target;
     this.setState({
       [name]: value,
     });
   }
+
+  render() {
+    return (
+    );
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(Login);
