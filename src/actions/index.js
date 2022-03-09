a04b63d27e66cefbfa3396bc6af3162c58e75428
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-09 19:45:29 -0300

    Criada action para salvar nome e email

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import { getTokenFromAPI } from '../services/API';
 
+export const ADD_EMAIL_NAME = 'ADD_EMAIL_NAME';
 export const SAVE_TOKEN = 'SAVE_TOKEN';
+
+export function addEmailAndNameToState(objectEmailName) {
+  return {
+    type: ADD_EMAIL_NAME,
+    payload: objectEmailName,
+  };
+}
 
 const saveTokenToState = (token) => ({
   type: SAVE_TOKEN,
