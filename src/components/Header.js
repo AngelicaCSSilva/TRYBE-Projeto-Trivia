ec1f9fbc36a565ad59e3a7fc547d75745182df5c
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-10 15:19:48 -0300

    Criação do componet Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+class Header extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Header);
