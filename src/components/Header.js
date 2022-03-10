e6c0d6c7817867b293cecbd4eb9ea80037ab2b10
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-10 15:21:01 -0300

    implementando o mapStateToProps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 class Header extends Component {
   render() {
+    const { emailFromUser, userName } = this.props;
     return (
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  emailFromUser: state.player.gravatarEmail,
+  userName: state.player.name,
+});
 export default connect(mapStateToProps)(Header);
