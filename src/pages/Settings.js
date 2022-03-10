38f0a611da95b0f00d4279a4e1670899dace0b42
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-10 15:15:42 -0300

    implementando pagina genérica de Settings

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1 data-testid="settings-title">Tela de configuração</h1>
+      </section>
+    );
+  }
+}
+
+export default Settings;
