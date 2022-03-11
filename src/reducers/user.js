4ba707cb86602d7aeec495eac54265be76c8d305
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:03:41 -0300

    criando reducer das perguntas

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,5 @@
-import { ADD_EMAIL_NAME, SAVE_TOKEN } from '../actions';
+import { ADD_EMAIL_NAME, SAVE_TOKEN,
+  GET_QUESTIONS_SUCCESS, GET_QUESTIONS_ERROR, REQUEST_QUESTIONS } from '../actions';
 
 const INITIAL_STATE = {
   name: '',
@@ -26,6 +27,25 @@ export function token(state = initialStateToken, action) {
   switch (action.type) {
   case SAVE_TOKEN:
     return action.token;
+  default:
+    return state;
+  }
+}
+
+const initialStateQuestions = {
+  isLoading: false,
+  questions: [],
+  errorMessage: '',
+};
+
+export function results(state = initialStateQuestions, action) {
+  switch (action.type) {
+  case REQUEST_QUESTIONS:
+    return { ...state, isLoading: true };
+  case GET_QUESTIONS_SUCCESS:
+    return { ...state, isLoading: false, questions: action.payload };
+  case GET_QUESTIONS_ERROR:
+    return { ...state, isLoading: false, errorMessage: action.payload };
   default:
     return state;
   }
