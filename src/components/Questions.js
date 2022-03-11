a320a0a2a65e8bbf240d1116e18447a241336397
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:15:12 -0300

    pegando as pergts com o token e salvando no estado

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,6 +3,13 @@ import { connect } from 'react-redux';
 
 class Questions extends Component {
 
+    componentDidMount = async () => {
+      const { token, saveAPIQuestions } = this.props;
+      await saveAPIQuestions(token);
+      const { results } = this.props;
+      this.setState({ allQuestions: results });
+    }
+
     render() {
       return (
         <section>
@@ -11,4 +18,15 @@ class Questions extends Component {
     }
 }
 
+Questions.propTypes = {
+  token: PropTypes.string.isRequired,
+  saveAPIQuestions: PropTypes.func.isRequired,
+  // saveAPIToken: PropTypes.func.isRequired,
+  results: PropTypes.objectOf(PropTypes.array).isRequired,
+  // statusCode: PropTypes.objectOf(PropTypes.array).isRequired,
+};
+const mapStateToProps = (state) => ({
+  token: state.token,
+  results: state.results.questions,
+});
 export default connect(mapStateToProps, mapDispatchToProps)(Questions);
