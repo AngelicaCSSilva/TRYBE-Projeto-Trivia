06a6653065e3cc3b17ee9dad55452064ee8d8640
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:27:48 -0300

    desculpem deu ruim aqui pra fazer esse ultm commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import { saveQuestions, saveToken } from '../actions';
+
+// const MIN = 0;
+// const MAX = 6;
+// const INDEX = Math.floor(Math.random() * (MAX - MIN) + MIN);
 
 class Questions extends Component {
+  state = {
+    currentQuestion: 0,
+    // answers: '',
+  }
 
     componentDidMount = async () => {
       const { token, saveAPIQuestions } = this.props;
       await saveAPIQuestions(token);
+    }
+
+    renderQuestion = () => {
       const { results } = this.props;
-      this.setState({ allQuestions: results });
+      const { currentQuestion } = this.state;
+      const { category, question } = results[currentQuestion];
+      return (
+        <div>
+          <p data-testid="question-category">{category}</p>
+          <p data-testid="question-text">{ question }</p>
+        </div>
+      );
+    }
+
+    renderAnswers = () => {
+      const { results } = this.props;
+      const { currentQuestion } = this.state;
+      const { category } = results[currentQuestion];
+      // const { currentQuestion, allQuestions } = this.state;
+      // const selectedQuestion = allQuestions[currentQuestion];
+      // console.log(allQuestions[currentQuestion].correct_answer);
+
+      // console.log(selectedQuestion.category);   nao consigo pegar as informaçoes
+      console.log(category);
+      // // const incorrectAnswers =
     }
 
     render() {
+      const { results } = this.props;
       return (
         <section>
+          { results.length > 0 && this.renderQuestion() }
+
+          { results.length > 0 && this.renderAnswers() }
         </section>
       );
     }
@@ -25,8 +62,15 @@ Questions.propTypes = {
   results: PropTypes.objectOf(PropTypes.array).isRequired,
   // statusCode: PropTypes.objectOf(PropTypes.array).isRequired,
 };
+
 const mapStateToProps = (state) => ({
   token: state.token,
   results: state.results.questions,
 });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   saveAPIQuestions: (token) => dispatch(saveQuestions(token)),
+//   saveAPIToken: () => dispatch(saveToken()),
+// });
+
 export default connect(mapStateToProps, mapDispatchToProps)(Questions);
