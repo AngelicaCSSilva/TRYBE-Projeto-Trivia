10dd7fd82e1bfb48e7ab630cae296561698bf679
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:01:11 -0300

    criando o fetch das perguuntas

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,11 @@
 export const getTokenFromAPI = async () => {
   const response = await fetch('https://opentdb.com/api_token.php?command=request');
   const data = await response.json();
-  console.log(data.token);
   return data.token;
 };
 
-export const getQuestions = async (param) => param;
+export const getQuestions = async (token) => {
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const data = await response.json();
+  return data;
+};
