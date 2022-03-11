066e8afd3160b925f10368c8b368b7c88e54fed7
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:05:14 -0300

    ligando componente questions a pagina game

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Questions from '../components/Questions';
 
 class Game extends Component {
   // handleChanges = ({ target }) => {
@@ -22,7 +23,7 @@ class Game extends Component {
           <Header />
         </header>
         <section>
-          <p> AAAA </p>
+          <Questions />
         </section>
       </>
     );
