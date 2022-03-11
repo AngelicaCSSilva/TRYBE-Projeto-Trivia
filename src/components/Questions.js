8177b58d7b0dcf6fc81a70a7dfb68cdeee598300
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-11 20:09:13 -0300

    criando componente generico

---
+++ src/components/Questions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Questions extends Component {
+
+    render() {
+      return (
+        <section>
+        </section>
+      );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Questions);
