8d3e622c69629563692c67a3b7cfc7f0e5fd1b17
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-12 17:01:31 -0300

    Importação actions

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { updateCountdown, resetCountdown, stopCountdown } from '../actions';
+
 class Countdown extends Component {
   render() {
     return (
