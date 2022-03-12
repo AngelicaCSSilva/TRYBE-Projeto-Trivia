d167a1b234415b840012f94fd7bbaf98d5f50493
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-12 16:57:06 -0300

    Finalizado reducer countdown com as actions

--- src/reducers/countdown.js
+++ src/reducers/countdown.js
@@ -1,3 +1,5 @@
+import { UPDATE_COUNTDOWN, STOP_COUNTDOWN, RESET_COUNTDOWN } from '../actions';
+
 const durationInSeconds = 30;
 
 const INITIAL_STATE = {
@@ -8,6 +10,22 @@ const INITIAL_STATE = {
 
 export function countdown(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case UPDATE_COUNTDOWN:
+    return {
+      ...state,
+      timer: state.timer - 1,
+    };
+  case STOP_COUNTDOWN:
+    return {
+      ...state,
+      isCountdownStopped: true,
+    };
+  case RESET_COUNTDOWN:
+    return {
+      ...state,
+      timer: state.durationInSeconds,
+      isCountdownStopped: false,
+    };
   default:
     return state;
   }
