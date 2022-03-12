c8f3a977addf68d619286a4c19e977697f41fdff
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-12 17:25:06 -0300

    Combinando reducer  randomlyAnswers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,6 @@ import { player } from './user';
 import { results } from './results';
 import { token } from './token';
 import { countdown } from './countdown';
+import { randomlyAnswers } from './randomlyAnswers';
 
-export default combineReducers({ player, token, results, countdown });
+export default combineReducers({ player, token, results, countdown, randomlyAnswers });
