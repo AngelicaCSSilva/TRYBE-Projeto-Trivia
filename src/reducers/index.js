09d51988107fd977836d1c89a4577fdacc4eec31
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-12 16:57:55 -0300

    Combinando reducer countdown

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import { player } from './user';
 import { results } from './results';
 import { token } from './token';
+import { countdown } from './countdown';
 
-export default combineReducers({ player, token, results });
+export default combineReducers({ player, token, results, countdown });
