cacd8f17a1b539608ea28acee70293fc8653c6f5
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-14 17:06:59 -0300

    Criando a pagina de feedback

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1 data-testid="feedback-text">Tela de feedback</h1>
+      </section>
+    );
+  }
+}
+
+export default Feedback;
