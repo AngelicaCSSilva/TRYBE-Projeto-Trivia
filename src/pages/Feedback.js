a5d65856315e3ebeb536b48117f87cb56b71e79c
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-14 20:26:17 -0300

    criando exibição da mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { userAssertions } = this.props;
+    console.log(userAssertions);
+    const MIN_POINTS = 2;
     return (
       <section>
-        <h1 data-testid="feedback-text">Tela de feedback</h1>
+        <Header />
+        <h1 data-testid="feedback-text">
+          { userAssertions <= MIN_POINTS ? 'Could be better...' : 'Well Done!'}
+        </h1>
       </section>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  userAssertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userAssertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
