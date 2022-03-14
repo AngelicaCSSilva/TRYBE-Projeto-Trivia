562c331b220a87d460eea1a13eee1c1fa6227d7e
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-14 20:37:23 -0300

    add exibição da userScore e acertos no Feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { userAssertions } = this.props;
+    const { userAssertions, userScore } = this.props;
     console.log(userAssertions);
     const MIN_POINTS = 2;
     return (
@@ -14,6 +14,17 @@ class Feedback extends React.Component {
         <h1 data-testid="feedback-text">
           { userAssertions <= MIN_POINTS ? 'Could be better...' : 'Well Done!'}
         </h1>
+
+        <label htmlFor="total-score">
+          Placar Final:
+          <p id="total-score" data-testid="feedback-total-score">{ userScore }</p>
+        </label>
+        <label htmlFor="total-question">
+          Respostas Corretas:
+          <p id="total-question" data-testid="feedback-total-question">
+            { userAssertions }
+          </p>
+        </label>
       </section>
     );
   }
@@ -21,10 +32,12 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   userAssertions: PropTypes.number.isRequired,
+  userScore: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   userAssertions: state.player.assertions,
+  userScore: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
