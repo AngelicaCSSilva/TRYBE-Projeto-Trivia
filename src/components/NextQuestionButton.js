7fa0e16e300b1ee12804f7cbadd4e42f8ad22883
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-14 17:03:03 -0300

    Criando componet do NextQuestionButton

---
+++ src/components/NextQuestionButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+class NextQuestionButton extends React.Component {
+ render() {
+   return (
+   );
+ }
+}
