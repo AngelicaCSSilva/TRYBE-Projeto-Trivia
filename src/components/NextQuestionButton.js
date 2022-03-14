9c0777e7b7a11b1436e35c6154f84c0c1694388b
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-14 17:08:10 -0300

    importando as actions pro NextQuestionButton

--- src/components/NextQuestionButton.js
+++ src/components/NextQuestionButton.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import {
+  currentQuestions,
+  redirecttofeedback,
+  clearRandomlyAnswers,
+  resetCountdown,
+} from '../actions';
+
 class NextQuestionButton extends React.Component {
  render() {
    return (
