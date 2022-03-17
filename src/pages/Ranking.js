8d476bf0d9535b93a30e5233ad92f07265245eb9
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-17 20:44:05 -0300

    Criando botão de retorno para a tela de login

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
     return (
       <section>
         <h1 data-testid="ranking-title">Tela de Ranking</h1>
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">
+            Inicio
+          </button>
+        </Link>
       </section>
     );
   }
