112b582846de8c13f00dd7b63f121332d1636287
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-18 21:00:55 -0300

    Implementado reset das questões

--- src/reducers/results.js
+++ src/reducers/results.js
@@ -1,5 +1,9 @@
 import {
-  GET_QUESTIONS_SUCCESS, GET_QUESTIONS_ERROR, REQUEST_QUESTIONS } from '../actions';
+  GET_QUESTIONS_SUCCESS,
+  GET_QUESTIONS_ERROR,
+  REQUEST_QUESTIONS,
+  RESET_QUESTIONS,
+} from '../actions';
 
 const initialStateQuestions = {
   isLoading: false,
@@ -15,6 +19,8 @@ export function results(state = initialStateQuestions, action) {
     return { ...state, isLoading: false, questions: action.payload };
   case GET_QUESTIONS_ERROR:
     return { ...state, isLoading: false, errorMessage: action.payload };
+  case RESET_QUESTIONS:
+    return { ...state, questions: [] };
   default:
     return state;
   }
