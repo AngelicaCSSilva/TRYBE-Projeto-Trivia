58a8dae6cb849d1834892c3e8d27469c83b2710f
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-23 18:01:52 -0300

    Styled Components - Feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { clearRandomlyAnswers } from '../actions';
 
+const FeedbackSectionStyled = styled.section`
+margin: 2vh;
+padding: 2vh;
+border-radius: 50px;
+background-color: rgb(250, 250, 250, 0.6);
+`;
+
+const ButtonStyled = styled.button`
+position: relative;
+  display: inline-block;
+  margin: 30px 15px;
+  padding: 15px 40px;
+  text-align: center;
+  font-size: 18px;
+  letter-spacing: 1px;
+  text-decoration: none;
+  color: #764ba2;
+  background: white;
+  transition: ease-out 0.5s;
+  border: 2px solid rgba(0,0,0,0.1);
+  border-radius: 50px;
+  -webkit-box-shadow: 8px 10px 31px 9px rgba(0,0,0,0.3); 
+  box-shadow: 8px 10px 31px 9px rgba(0,0,0,0.3);
+`;
 class Feedback extends React.Component {
   componentDidMount() {
     document.title = 'Trivia - Resultado';
@@ -16,38 +41,39 @@ class Feedback extends React.Component {
     const { userAssertions, userScore } = this.props;
     const MIN_POINTS = 2;
     return (
-      <section>
+      <>
         <Header />
-        <h1 data-testid="feedback-text">
-          { userAssertions <= MIN_POINTS ? 'Could be better...' : 'Well Done!'}
-        </h1>
-
-        <label htmlFor="total-score">
-          Placar Final:
-          <p id="total-score" data-testid="feedback-total-score">{ userScore }</p>
-        </label>
-        <label htmlFor="total-question">
-          Respostas Corretas:
-          <p id="total-question" data-testid="feedback-total-question">
-            { userAssertions }
-          </p>
-        </label>
+        <FeedbackSectionStyled>
+          <h1 data-testid="feedback-text">
+            { userAssertions <= MIN_POINTS ? 'Could be better...' : 'Well Done!'}
+          </h1>
+          <label htmlFor="total-score">
+            Placar Final:
+            <p id="total-score" data-testid="feedback-total-score">{ userScore }</p>
+          </label>
+          <label htmlFor="total-question">
+            Respostas Corretas:
+            <p id="total-question" data-testid="feedback-total-question">
+              { userAssertions }
+            </p>
+          </label>
+        </FeedbackSectionStyled>
         <Link to="/">
-          <button type="button" data-testid="btn-play-again">
+          <ButtonStyled type="button" data-testid="btn-play-again">
             Play Again
-          </button>
+          </ButtonStyled>
         </Link>
 
         <Link to="/ranking">
-          <button
+          <ButtonStyled
             type="button"
             data-testid="btn-ranking"
             onClick={ this.hendleRanking }
           >
             Ranking
-          </button>
+          </ButtonStyled>
         </Link>
-      </section>
+      </>
     );
   }
 }
