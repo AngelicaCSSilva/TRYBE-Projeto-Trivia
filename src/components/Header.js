336c06a72c098e300ef8ca51653176bb9889f430
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-23 18:02:09 -0300

    Styled Components - Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 
+const FlexDiv = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-evenly;
+align-items: center;
+align-content: center;
+margin: 2vh;
+padding: 5px;
+height: 60px;
+border-radius: 50px;
+background-color: rgb(250, 250, 250, 0.6);
+`;
+
+const Avatar = styled.img`
+width: 40px;
+border-radius: 100%;
+@media ( min-width : 768px ) {
+  width: 60px;
+}
+`;
 class Header extends Component {
   render() {
     const { emailFromUser, userName, userScore } = this.props;
     const hash = md5(emailFromUser).toString();
     return (
-      <>
-        <img
+      <FlexDiv>
+        <Avatar
           src={ `https://www.gravatar.com/avatar/${hash}` }
           alt="imagem do usuario"
           data-testid="header-profile-picture"
         />
         <p data-testid="header-player-name">
-          { `Nome de suário: ${userName}` }
+          { userName }
         </p>
         <label htmlFor="header-score">
           Placar:
-          <p id="header-score" data-testid="header-score">{ userScore }</p>
+          {' '}
+          <span id="header-score" data-testid="header-score">{ userScore }</span>
         </label>
-      </>
+      </FlexDiv>
     );
   }
 }
