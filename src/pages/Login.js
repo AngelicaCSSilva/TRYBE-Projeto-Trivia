a162140821af446e0c568d9526b434dcb68e3649
Angélica Silva <angelica.silva9323@gmail.com>
2022-03-23 18:00:45 -0300

    Styled Components - Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -10,6 +11,76 @@ import {
   resetQuestions,
   resetUser,
 } from '../actions';
+
+// [ Styled.Components ]
+// ** Header
+const StyledHeader = styled.header`
+  margin-top: 20vh;
+`;
+
+const StyledLogo = styled.img`
+width: 80%;
+@media ( min-width : 768px ) {
+  width: 65%;
+}
+@media ( min-width : 1024px ) {
+  width: 50%;
+}
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: center;
+  flex-direction: column;
+  margin-top: 20px;
+  `;
+
+const Input = styled.input`
+  border: solid 1px #ccc;
+  margin: 4px;
+  padding: 5px;
+  border-radius: 10px;
+  
+  &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  `;
+
+// Ref.: https://cssbuttons.io/detail/adamgiebl/rude-bear-14
+const StyledButton = styled.button`
+  position: relative;
+  display: inline-block;
+  margin: 30px 15px;
+  padding: 15px 40px;
+  text-align: center;
+  font-size: 18px;
+  letter-spacing: 1px;
+  text-decoration: none;
+  color: #764ba2;
+  background: white;
+  transition: ease-out 0.5s;
+  border: 2px solid rgba(0,0,0,0.1);
+  border-radius: 50px;
+  -webkit-box-shadow: 8px 10px 31px 9px rgba(0,0,0,0.3); 
+  box-shadow: 8px 10px 31px 9px rgba(0,0,0,0.3);
+
+  &:disabled{
+    border: 2px solid #bbb;
+    color: #aaa;
+  }
+  `;
+
+const PlayButton = styled(StyledButton).attrs({
+  id: 'btn-play',
+})`
+  &:enabled {
+    color: white;
+    background-color: #764ba2;
+  }`;
 
 class Login extends Component {
   state = {
@@ -53,47 +124,50 @@ class Login extends Component {
     const { email, name } = this.state;
     return (
       <section>
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            SUA VEZ
-          </p>
-        </header>
+        <StyledHeader>
+          <StyledLogo
+            src={ logo }
+            alt="logo"
+          />
+        </StyledHeader>
         <form>
-          <label htmlFor="name">
+          <StyledLabel htmlFor="name">
             Nome
-            <input
+            <Input
               id="name"
               name="name"
               data-testid="input-player-name"
               onChange={ this.handleChanges }
+              placeholder="Digite o seu nome..."
               value={ name }
             />
-          </label>
-          <label htmlFor="email">
+          </StyledLabel>
+          <StyledLabel htmlFor="email">
             Email
-            <input
+            <Input
               id="email"
               name="email"
               data-testid="input-gravatar-email"
+              placeholder="Digite o seu email..."
               onChange={ this.handleChanges }
               value={ email }
             />
-          </label>
-          <button
+          </StyledLabel>
+          <PlayButton
             data-testid="btn-play"
             type="button"
+            id="btn-play"
             disabled={
               name.length === 0 || email.length === 0 || !this.validateEmail(email)
             }
             onClick={ this.handleClick }
           >
             Play
-          </button>
+          </PlayButton>
           <Link to="/settings">
-            <button type="button" data-testid="btn-settings">
-              configuração
-            </button>
+            <StyledButton type="button" data-testid="btn-settings">
+              Configuração
+            </StyledButton>
           </Link>
         </form>
       </section>
